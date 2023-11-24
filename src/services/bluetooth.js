d4692915f868174b03d307b13e376c4189d694d0
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-11-24 15:33:42 +0100

    WIP

--- src/services/bluetooth.js
+++ src/services/bluetooth.js
@@ -1,26 +1,32 @@
-export async function connectBluetooth({ bluetoothServices }) {
+export async function connectBluetooth(bluetoothOptions) {
     if (!navigator.bluetooth) {
         throw new Error('Bluetooth is not available.');
     }
 
-    if (!Array.isArray(bluetoothServices)) {
-        throw new Error('Invalid input: bluetoothServices must be an array');
-    }
-
     try {
         const device = await navigator.bluetooth.requestDevice({
-            acceptAllDevices: true,
-            optionalServices: bluetoothServices.map(service => service.key),
+            acceptAllDevices: bluetoothOptions.acceptAllDevices,
+            optionalServices: bluetoothOptions.bluetoothServices.map(service => service.key),
         });
 
         const server = await device.gatt.connect();
-
         const services = await server.getPrimaryServices();
 
         let characteristicsMap = {};
         for (const service of services) {
             const characteristics = await service.getCharacteristics();
-            characteristicsMap[service.uuid] = characteristics;
+            characteristicsMap[service.uuid] = [];
+
+            for (const characteristic of characteristics) {
+                let charDetails = { uuid: characteristic.uuid, properties: characteristic.properties, value: null };
+
+                if (characteristic.properties.read) {
+                    charDetails.value = await characteristic.readValue();
+                    // Convert the value to a proper format if necessary
+                }
+
+                characteristicsMap[service.uuid].push(charDetails);
+            }
         }
 
         return { device, services: characteristicsMap };
