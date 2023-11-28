9268bc8b21403b1f8930668736b66485c05244f8
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-11-28 15:10:51 +0100

    Refactor Bluetooth component and service to improve readability and maintainability
    The Bluetooth component and service have been refactored to enhance code
    readability and maintainability. The Bluetooth component's service
    options have been re-arranged to improve their logical order.
    Additionally, the Bluetooth service now includes helper functions to
    convert characteristic properties and values, making the code more
    modular and reusable. These changes contribute to better code
    organization and understanding. No functional changes are expected.
    
    #N/A

--- src/services/bluetooth.js
+++ src/services/bluetooth.js
@@ -1,7 +1,30 @@
+function convertCharacteristicProperties(properties) {
+    let readableProperties = [];
+    for (const key in properties) {
+        if (properties[key] === true) {
+            readableProperties.push(key);
+        }
+    }
+    return readableProperties.join(', ');
+}
+
+function convertCharacteristicValue(dataView) {
+    let valueHex = '';
+    for (let i = 0; i < dataView.byteLength; i++) {
+        valueHex += dataView.getUint8(i).toString(16).padStart(2, '0');
+    }
+    return valueHex;
+}
+
 export async function connectBluetooth({ bluetoothServices }) {
     if (!navigator.bluetooth) {
         throw new Error('Bluetooth is not available.');
     }
+
+    const serviceLabels = bluetoothServices.reduce((map, service) => {
+        map[service.key] = service.label;
+        return map;
+    }, {});
 
     try {
         const device = await navigator.bluetooth.requestDevice({
@@ -15,21 +38,29 @@ export async function connectBluetooth({ bluetoothServices }) {
         let characteristicsMap = {};
         for (const service of services) {
             const characteristics = await service.getCharacteristics();
-            characteristicsMap[service.uuid] = [];
+            const serviceName = serviceLabels[service.uuid] || `Unknown Service (${service.uuid})`;
+
+            characteristicsMap[serviceName] = [];
 
             for (const characteristic of characteristics) {
-                let charDetails = { uuid: characteristic.uuid, properties: characteristic.properties, value: null };
+                let charDetails = {
+                    uuid: characteristic.uuid,
+                    properties: convertCharacteristicProperties(characteristic.properties),
+                    value: null,
+                    humanReadableValue: null,
+                };
 
                 if (characteristic.properties.read) {
-                    charDetails.value = await characteristic.readValue();
-                    // Convert the value to a proper format if necessary
+                    const rawValue = await characteristic.readValue();
+                    charDetails.value = convertCharacteristicValue(rawValue);
+                    // Here we can add specific conversions for known characteristic types if necessary
                 }
 
-                characteristicsMap[service.uuid].push(charDetails);
+                characteristicsMap[serviceName].push(charDetails);
             }
         }
 
-        return { device, services: characteristicsMap };
+        return { deviceName: device.name, services: characteristicsMap };
     } catch (error) {
         throw new Error(error, 'Error while connecting to Bluetooth device.');
     }
