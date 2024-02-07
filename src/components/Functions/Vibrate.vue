c3a38c967dd3679e949c577ed8490e5d61692d33
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-02-07 05:18:24 +0100

    Fix

--- src/components/Functions/Vibrate.vue
+++ src/components/Functions/Vibrate.vue
@@ -1,14 +1,15 @@
 <template>
-    <wwEditorInputRow
-        label="Vibration Pattern"
-        type="array"
-        :model-value="vibrate_pattern"
-        bindable
-        required
-        placeholder="Enter vibration pattern"
-        @update:modelValue="setPattern"
-    />
-    <wwEditorQuestionMark tooltip-position="top-left" class="ml-2" :forcedContent="vibrationHelp" />
+    <div>
+        <wwEditorInputRow
+            label="Vibration Pattern"
+            type="array"
+            :model-value="vibrate_pattern"
+            bindable
+            required
+            placeholder="Enter vibration pattern"
+            @update:modelValue="setPattern"
+        />
+    </div>
 </template>
 
 <script>
@@ -18,11 +19,6 @@ export default {
         args: { type: Object, required: true },
     },
     emits: ['update:args'],
-    data() {
-        return {
-            vibrationHelp: `The vibration pattern is an array of numbers that describes the pattern with which the device should vibrate: [200, 500, 200, 500]. <br/>The values are in milliseconds. The first value indicates the number of milliseconds to wait before turning the vibration on. The next value indicates the number of milliseconds for which to keep the vibration on before turning it off.  <br/>Subsequent values alternate between the number of milliseconds to turn the vibration off or to turn the vibration on.`,
-        };
-    },
     computed: {
         vibrate_pattern() {
             return this.args.vibrate_pattern;
