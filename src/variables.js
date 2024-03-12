9ebea37e8e8119f34a853aed7a0f44f9c8be7852
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-03-12 12:24:16 +0100

    test

--- src/variables.js
+++ src/variables.js
@@ -215,7 +215,6 @@ export const listenPwa = pluginId => {
         isInstalled = installed;
         wwLib.wwVariable.updateValue(`${pluginId}-isPwaInstalled`, {
             isInstalled,
-            supported: 'serviceWorker' in navigator,
             info: 'iOS Safari may not reliably report the installed state of this PWA. Please be aware of potential limitations in tracking its installation status on iOS devices.',
         });
     });
